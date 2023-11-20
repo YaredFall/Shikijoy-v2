145855d35de8380ca31ea34a35f5fdd4fe142d2e
YaredFall <yaredfall@gmail.com>
2023-11-20 18:28:25 +0700

    client init

---
+++ client/src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+
+const appEL = document.createElement("div");
+appEL.setAttribute("id", "app");
+document.body.appendChild(appEL);
+
+ReactDOM.createRoot(document.getElementById('app')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
