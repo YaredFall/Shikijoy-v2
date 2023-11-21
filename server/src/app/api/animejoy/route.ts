ac304111be1ca6c3ab4f2ca4afe7a588151457f7
YaredFall <yaredfall@gmail.com>
2023-11-21 16:20:45 +0700

    animejoy dev mode api route

---
+++ server/src/app/api/animejoy/route.ts
@@ -0,0 +1,17 @@
+import { LINKS } from "@/utils";
+import got from "got";
+import { NextRequest } from "next/server";
+
+export const dynamic = 'force-dynamic';
+
+export async function GET(request: NextRequest) {
+    const search = request.nextUrl.search;
+    if (!search.toString().includes("?url=")) {
+        return new Response("Please specify the URL in the 'url' query string.", { status: 400 });
+    }
+    const url = search.replace("?url=", "");
+
+    const { headers, body } = (await got(`${LINKS.pupflare}/?url=${LINKS.animejoy}${url}`));
+
+    return new Response(body, { headers: headers as HeadersInit });
+}
---
+++ server/src/utils.ts
@@ -0,0 +1,4 @@
+export const LINKS = {
+    pupflare: "http://localhost:8080",
+    animejoy: "https://animejoy.ru"
+} as const
