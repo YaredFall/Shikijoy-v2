ef8e8da9aad0aa43cf22e7a3c8895f3856042e6d
YaredFall <yaredfall@gmail.com>
2023-11-23 19:03:00 +0700

    allowed cross origin request; disabled trailing slash redirects

---
+++ server/src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse, NextRequest } from "next/server";
+import { allowCORS } from "../middlewares/allow-cors";
+
+export default function middleware(request: NextRequest) {
+  console.log(request.nextUrl.pathname);
+
+  let response = NextResponse.next();
+
+  if (request.nextUrl.pathname.startsWith("/api")) {
+    response = allowCORS(request, response);
+  }
+
+  return response;
+}
