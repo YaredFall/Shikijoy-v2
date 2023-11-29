1f0ea7d563479d45ba2df7f68aea09b857e4c497
YaredFall <yaredfall@gmail.com>
2023-11-30 03:08:56 +0700

    fixed #app el duplication

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-const appEL = document.createElement("div");
-appEL.setAttribute("id", "app");
-document.body.appendChild(appEL);
+let appEL = document.getElementById("app");
+if (!appEL) {
+  appEL = document.createElement("div");
+  appEL.setAttribute("id", "app");
+  document.body.appendChild(appEL);
+}
 
 ReactDOM.createRoot(document.getElementById("app")!).render(
   <React.StrictMode>
