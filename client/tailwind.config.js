efd5d2be54b4f47fdf0ecfb18e286444f1815304
YaredFall <yaredfall@gmail.com>
2023-11-30 02:56:32 +0700

    tailwind config colors setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin")
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,7 +7,36 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "rgb(247, 247, 247)"
+        },
+        secondary: "rgb(48, 48, 48)",
+        tertiary: "rgb(36, 36, 36)",
+        accent: {
+          "primary": "rgb(255, 123, 121)",
+          "secondary": "rgb(116, 200, 200)"
+        },
+        danger: "rgb(255, 123, 121)"
+      },
+      opacity: {
+        ".75": ".75",
+        ".5": ".50",
+        ".25": ".25",
+        ".125": ".125",
+        ".0625": ".0625"
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addBase, theme }) {
+      addBase({
+        "html": {
+          "@apply bg-tertiary text-primary": "",
+          "color-scheme": "dark",
+        },
+      })
+    })
+  ]
 }
