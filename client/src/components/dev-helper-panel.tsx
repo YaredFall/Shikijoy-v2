d7bafb5b6064fc7ec753dfcff48e3bf3943223ea
YaredFall <yaredfall@gmail.com>
2024-01-03 21:48:46 +0700

    extracted dev helper panel

---
+++ client/src/components/dev-helper-panel.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
+type DevHelperPanelProps = {};
+
+//3162 - studio + different players
+//2611 - single studio + episode sets
+//595 - no studios + episode sets
+//2308 - no groups
+const ids = [3162, 2611, 595, 2308, 1006];
+
+export default function DevHelperPanel({ }: DevHelperPanelProps) {
+
+  const navigate = useNavigate();
+
+  if (!import.meta.env.DEV) return null;
+
+  return (
+    <div className="flex gap-4 fixed bottom-0 px-4 py-2 bg-secondary">
+      {ids.map(id => (<button key={id} onClick={() => navigate(`/tv-serialy/${id}-.html`)}>{id}</button>))}
+    </div>
+  );
+}
--- client/src/components/pages/show/show-page.tsx
+++ client/src/components/pages/show/show-page.tsx
@@ -10,15 +10,9 @@ type ShowPageProps = {};
 
 export default function ShowPage({ }: ShowPageProps) {
 
-  //3162 - studio + different players
-  //2611 - single studio + episode sets
-  //595 - no studios + episode sets
-  //2308 - no groups
-  const ids = [3162, 2611, 595, 2308, 1006];
-  
   const navigate = useNavigate();
   const location = useLocation();
-  
+
   const { data, isLoading: isLoadingPage } = useAnimejoyPage(undefined, (data) => {
     if (data) {
       document.title = data.page.title;
@@ -27,15 +21,11 @@ export default function ShowPage({ }: ShowPageProps) {
       }
     }
   });
-  
-  
+
   const showTitle = getShowTitle(data?.page);
-  
+
   return (
     <main className="px-8 py-6 max-w-7xl flex flex-col gap-4">
-      <div className="flex gap-4 fixed bottom-0 px-4 py-2 bg-secondary">
-        {ids.map(id => (<button key={id} onClick={() => navigate(`/tv-serialy/${id}-.html`)}>{id}</button>))}
-      </div>
       <header>
         <LoadableText as="h1" isLoading={isLoadingPage} placeholderLength={40} className="font-medium text-2xl">{showTitle?.ru}</LoadableText>
         <LoadableText as="h2" isLoading={isLoadingPage} placeholderLength={30} className="font-medium text-lg text-primary/.5">{showTitle?.romanji}</LoadableText>
