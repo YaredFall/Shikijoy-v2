ec540ad10398ce3a9896f03b076cadb468d97680
YaredFall <yaredfall@gmail.com>
2024-01-03 21:45:04 +0700

    image with preloader

---
+++ client/src/components/ui/image.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils";
+import React, { useCallback, useEffect, useState } from "react";
+
+type ImageProps = React.ImgHTMLAttributes<HTMLImageElement>;
+
+export default function Image({ className, src, onLoad, onError, alt, ...other}: ImageProps) {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handler = useCallback((passedHandler?: React.ReactEventHandler<HTMLImageElement>) => (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    passedHandler?.(e);
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    setIsLoading(true);
+  }, [src]);
+
+  return (
+    <div className={cn(className, "relative")}>
+      <img src={src} className={cn(className, isLoading && "hidden")} {...other} alt={alt} onLoad={handler(onLoad)} onError={handler(onError)}/>
+      {isLoading && <div aria-label={`(Загружается...) ${alt}`} className={cn(className, "bg-neutral-500 absolute inset-0 animate-pulse")} />}
+    </div>
+  );
+}
