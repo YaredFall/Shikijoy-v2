2dd62da037d10238279a527c1949ddb3ed117e82
YaredFall <yaredfall@gmail.com>
2024-01-21 16:47:05 +0700

    brought back styles (removed import accidentally)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import DevHelperPanel from "@/components/dev-helper-panel";
 import Header from "@/components/nav/header";
 import CategoryPage from "@/components/pages/category/category-page";
 import ShowPage from "@/components/pages/show/show-page";
+import "@/index.css";
 import { HOME_AS_CATEGORY, SHOW_CATEGORIES } from "@/utils/routing";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
