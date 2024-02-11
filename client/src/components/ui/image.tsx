7c15fe9d8c006befae05d117055dd63967a7112a
YaredFall <yaredfall@gmail.com>
2024-02-11 13:37:55 +0700

    `client` image component loading state fix

--- client/src/components/ui/image.tsx
+++ client/src/components/ui/image.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useLayoutEffect, useState } from "react";
 
 type ImageProps = React.ImgHTMLAttributes<HTMLImageElement>;
 
@@ -9,16 +9,25 @@ export default function Image({ className, src, onLoad, onError, alt, ...other }
 
     const handler = useCallback((passedHandler?: React.ReactEventHandler<HTMLImageElement>) => (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
         passedHandler?.(e);
-        setIsLoading(false);
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 0);
     }, []);
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         setIsLoading(true);
     }, [src]);
 
     return (
         <div className={cn(className, "relative")}>
-            <img src={src} className={cn(className, isLoading && "hidden")} {...other} alt={alt} onLoad={handler(onLoad)} onError={handler(onError)} />
+            <img
+                src={src}
+                className={cn("object-cover", className, isLoading && "hidden")}
+                {...other}
+                alt={alt}
+                onLoad={handler(onLoad)}
+                onError={handler(onError)}
+            />
             {isLoading && <div aria-label={`(Загружается...) ${alt}`} className={cn(className, "bg-neutral-500 absolute inset-0 animate-pulse")} />}
         </div>
     );
