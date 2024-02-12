0de59c6d5ab6911a3f5edf91ef5b3269e6fd37d3
YaredFall <yaredfall@gmail.com>
2024-02-12 22:47:53 +0700

    `client` hover-popover (improved)

---
+++ client/src/components/ui/hover-popover.tsx
@@ -0,0 +1,152 @@
+import useDebounced from "@/hooks/useDebounced";
+import { cn } from "@/lib/utils";
+import { createContext } from "@/utils/context";
+import { Slot } from "@radix-ui/react-slot";
+import {
+    ComponentPropsWithoutRef,
+    ReactNode,
+    forwardRef,
+    useCallback,
+    useId,
+    useImperativeHandle,
+    useMemo,
+    useRef,
+    useState,
+} from "react";
+
+type HoverPopoverContext = {
+    isOpen: boolean;
+    setIsOpen: (newOpen: boolean) => void;
+    id: string;
+};
+
+const [useHoverPopoverContext, HoverPopoverContextProvider] = createContext<HoverPopoverContext>("HoverPopover", {} as HoverPopoverContext);
+
+type HoverPopoverProps = {
+    defaultOpen?: boolean;
+    open?: boolean;
+    onOpenChange?: (newOpen: boolean) => void;
+    children?: ReactNode;
+    asChild?: boolean;
+    debounceTime?: number;
+} & ComponentPropsWithoutRef<"div">;
+
+type HoverPopoverElement = React.ElementRef<"div">;
+
+export const HoverPopover = forwardRef<HoverPopoverElement, HoverPopoverProps>(({ asChild, defaultOpen, open, onOpenChange, children, className, debounceTime = 500, ...other }, forwardRef) => {
+
+    const Comp = asChild ? Slot : "div";
+
+    const nodeRef = useRef<HTMLDivElement>(null);
+    useImperativeHandle(forwardRef, () => nodeRef.current!);
+    const id = "yd-hover-popover-" + useId();
+
+    const [_isOpen, _setIsOpen] = useState(defaultOpen ?? open ?? false);
+
+    const isOpen = useMemo(() => open ?? _isOpen, [_isOpen, open]);
+    const debouncedIsOpen = useDebounced(isOpen, debounceTime);
+
+    const setIsOpen = useCallback((newOpen: boolean) => {
+        _setIsOpen(newOpen);
+        onOpenChange?.(newOpen);
+    }, [onOpenChange]);
+
+    return (
+        <HoverPopoverContextProvider id={id} isOpen={debouncedIsOpen} setIsOpen={setIsOpen}>
+            <Comp ref={nodeRef} className={cn("relative", className)} {...other} yd-hover-popover-id={id}>
+                {children}
+            </Comp>
+        </HoverPopoverContextProvider>
+    );
+});
+
+
+type HoverPopoverTriggerElement = React.ElementRef<"button">;
+
+type HoverPopoverTriggerProps = Omit<ComponentPropsWithoutRef<"button">, "children"> & {
+    asChild?: boolean;
+    children?: ReactNode | ((isOpen: boolean) => ReactNode);
+};
+export const HoverPopoverTrigger = forwardRef<HoverPopoverTriggerElement, HoverPopoverTriggerProps>(({ asChild, children, onMouseEnter, onMouseLeave, onFocus, onBlur, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+
+    const { isOpen, setIsOpen, id } = useHoverPopoverContext("HoverPopoverTrigger");
+
+    const onMouseEnterHandler: React.MouseEventHandler<HTMLButtonElement> = useCallback((e) => {
+        onMouseEnter?.(e);
+        setIsOpen(true);
+    }, [onMouseEnter, setIsOpen]);
+    const onFocusHandler: React.FocusEventHandler<HTMLButtonElement> = useCallback((e) => {
+        onFocus?.(e);
+        setIsOpen(true);
+    }, [onFocus, setIsOpen]);
+    const onMouseLeaveHandler: React.MouseEventHandler<HTMLButtonElement> = useCallback((e) => {
+        onMouseLeave?.(e);
+        setIsOpen(false);
+    }, [onMouseLeave, setIsOpen]);
+    const onBlurHandler: React.FocusEventHandler<HTMLButtonElement> = useCallback((e) => {
+        onBlur?.(e);
+        setIsOpen(false);
+    }, [onBlur, setIsOpen]);
+
+    return (
+        <Comp
+            ref={ref}
+            role={"button"}
+            id={id + "-trigger"}
+            onMouseEnter={onMouseEnterHandler}
+            onMouseLeave={onMouseLeaveHandler}
+            onFocus={onFocusHandler}
+            onBlur={onBlurHandler}
+            {...props}
+        >
+            {children instanceof Function ? children(isOpen) : children}
+        </Comp>
+    );
+});
+
+type HoverPopoverContentElement = React.ElementRef<"div">;
+
+type HoverPopoverContentProps = Omit<ComponentPropsWithoutRef<"div">, "children"> & {
+    asChild?: boolean;
+    children?: ReactNode | ((isOpen: boolean) => ReactNode);
+};
+export const HoverPopoverContent = forwardRef<HoverPopoverContentElement, HoverPopoverContentProps>(({ asChild, children, onMouseEnter, onFocus, onMouseLeave, onBlur, ...props }, ref) => {
+    const Comp = asChild ? Slot : "div";
+
+    const { isOpen, setIsOpen, id } = useHoverPopoverContext("HoverPopoverContent");
+
+    const onMouseEnterHandler: React.MouseEventHandler<HTMLDivElement> = useCallback((e) => {
+        onMouseEnter?.(e);
+        setIsOpen(true);
+    }, [onMouseEnter, setIsOpen]);
+    const onFocusHandler: React.FocusEventHandler<HTMLDivElement> = useCallback((e) => {
+        onFocus?.(e);
+        setIsOpen(true);
+    }, [onFocus, setIsOpen]);
+    const onMouseLeaveHandler: React.MouseEventHandler<HTMLDivElement> = useCallback((e) => {
+        onMouseLeave?.(e);
+        setIsOpen(false);
+    }, [onMouseLeave, setIsOpen]);
+    const onBlurHandler: React.FocusEventHandler<HTMLDivElement> = useCallback((e) => {
+        onBlur?.(e);
+        setIsOpen(false);
+    }, [onBlur, setIsOpen]);
+
+    return (
+        isOpen && (
+            <Comp
+                ref={ref}
+                role={"dialog"}
+                aria-labelledby={id + "-trigger"}
+                onMouseEnter={onMouseEnterHandler}
+                onMouseLeave={onMouseLeaveHandler}
+                onFocus={onFocusHandler}
+                onBlur={onBlurHandler}
+                {...props}
+            >
+                {children instanceof Function ? children(isOpen) : children}
+            </Comp>
+        )
+    );
+});
