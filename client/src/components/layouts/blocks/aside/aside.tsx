4016b5e0ac0d803e87abc0bc7c85be6745c2bd78
YaredFall <yaredfall@gmail.com>
2024-02-21 23:31:17 +0700

    `cleint` fixed menu popover (moved to headlessui); tweaked quick-search bg

--- client/src/components/layouts/blocks/aside/aside.tsx
+++ client/src/components/layouts/blocks/aside/aside.tsx
@@ -7,7 +7,7 @@ const Aside = forwardRef<HTMLDivElement, AsideProps>(({ children }, ref) => {
     return (
         <aside ref={ref} className={"w-aside-width max-h-full fixed inset-y-0 right-0 overflow-hidden shrink-0 px-1.5 z-10 flex flex-col pb-1.5"}>
             <div className={"h-breadcrumbs-height shrink-0 py-1.5 bg-background-fill"}>
-                <QuickSearch className={"h-full bg-background-primary rounded-md bg-gradient-to-t from-white/5 to-transparent"} />
+                <QuickSearch className={"h-full bg-background-primary rounded-md bg-gradient-to-t from-background-tertiary/15 to-transparent"} />
             </div>
             <div className={"h-full shadow-md bg-background-primary rounded-md "}>
                 {children}
--- client/src/components/layouts/blocks/header/menu.tsx
+++ client/src/components/layouts/blocks/header/menu.tsx
@@ -1,41 +1,34 @@
 // import Popover from "@/components/ui/popover";
 import { cn } from "@/lib/utils";
 import { HOME_AS_CATEGORY, SHOW_CATEGORIES } from "@/utils/routing";
-import { HTMLProps } from "react";
+import { ComponentProps, HTMLProps } from "react";
 import { Link } from "@/components/utility/Link";
-import * as Popover from "@radix-ui/react-popover";
+import { Popover } from "@headlessui/react";
 
-type MenuProps = Omit<HTMLProps<HTMLDivElement>, "children">;
+type MenuProps = Omit<ComponentProps<typeof Popover>, "children">;
 
 export default function Menu({ className, ...other }: MenuProps) {
     return (
-        <div className={cn("relative", className)} {...other}>
-            <Popover.Root>
-                <Popover.Trigger className={"w-full h-full"}>Меню</Popover.Trigger>
-                <Popover.Content
-                    side={"right"}
-                    align={"start"}
-                    className={"w-52 px-4 py-4 bg-secondary rounded-r text-sm"}
-                    onOpenAutoFocus={(e) => {
-                        e.preventDefault();
-                    }}
-                >
-                    <nav className={"flex flex-col gap-4"}>
-                        {
-                            [HOME_AS_CATEGORY, ...SHOW_CATEGORIES.filter(c => c.path !== "dorams")].map(({ path, name }) => (
-                                <Link
-                                    key={name}
-                                    absolute
-                                    to={path}
-                                    className={"pt-2.5 pb-1.5 px-4 flex items-center text-foreground-primary/.75 highlight:text-foreground-primary"}
-                                >
-                                    {name}
-                                </Link>
-                            ))
-                        }
-                    </nav>
-                </Popover.Content>
-            </Popover.Root>
-        </div>
+        <Popover className={cn("relative", className)} {...other}>
+            <Popover.Button className={"w-full h-full"}>Меню</Popover.Button>
+            <Popover.Panel
+                className={"absolute left-full top-0 w-52 bg-background-primary rounded-r-md text-sm overflow-hidden"}
+            >
+                <nav className={"bg-gradient-to-b from-accent-primary/5 to-background-secondary/50 flex flex-col gap-4 px-4 py-4"}>
+                    {
+                        [HOME_AS_CATEGORY, ...SHOW_CATEGORIES.filter(c => c.path !== "dorams")].map(({ path, name }) => (
+                            <Link
+                                key={name}
+                                absolute
+                                to={path}
+                                className={"pt-2.5 pb-1.5 px-4 flex items-center text-foreground-primary/.75 highlight:text-foreground-primary"}
+                            >
+                                {name}
+                            </Link>
+                        ))
+                    }
+                </nav>
+            </Popover.Panel>
+        </Popover>
     );
 }
