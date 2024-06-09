7ebbe6b5b3684d5b819712599194c35526f46618
YaredFall <yaredfall@gmail.com>
2024-06-09 13:50:08 +0700

    `server` *chore* changed next eslint config

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -1,6 +1,13 @@
 const path = require("path")
 
+/**
+ * @type {import("eslint").Linter.Config}
+ */
 module.exports = {
+    extends: [
+        "../.eslintrc.cjs",
+        "plugin:@next/next/recommended",
+    ],
     settings: {
         tailwindcss: {
             config: path.join(__dirname, "./tailwind.config.ts"),
