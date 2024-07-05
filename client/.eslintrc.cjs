215a251d2cd5a41c489a18672b97ff8676c6c58e
YaredFall <yaredfall@gmail.com>
2024-07-05 23:02:28 +0700

    `client`: fixed eslint tailwind plugin, config formatting

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,19 +1,26 @@
+const path = require("path");
+
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "../.eslintrc.cjs",
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        "../.eslintrc.cjs",
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
     ],
-  },
-}
+    settings: {
+        tailwindcss: {
+            config: path.join(__dirname, "./tailwind.config.ts"),
+        },
+    },
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+    },
+};
