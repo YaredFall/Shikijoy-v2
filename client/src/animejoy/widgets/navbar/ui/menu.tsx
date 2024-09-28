36f233c50e5b3c90b4b1f4781ead0ca0f8efccde
YaredFall <yaredfall@gmail.com>
2024-09-29 01:39:40 +0700

    feat(client): working on navbar (menu)

---
+++ client/src/animejoy/widgets/navbar/ui/menu.tsx
@@ -0,0 +1,25 @@
+// import Popover from "@/components/ui/popover";
+import { cn } from "@/shared/lib/cn";
+import { CATEGORIES, categoryLabel } from "@/shared/routing/category";
+import { Link } from "@tanstack/react-router";
+import { ComponentProps } from "react";
+
+type MenuProps = Omit<ComponentProps<"nav">, "children">;
+
+export default function Menu({ className }: MenuProps) {
+    return (
+        <nav className={cn("flex flex-col gap-3 bg-gradient-to-b from-accent-primary/5 to-background-secondary/50 p-4", className)}>
+            {
+                CATEGORIES.map(category => (
+                    <Link
+                        key={category}
+                        to={category}
+                        className={"flex items-center px-4 pb-2 pt-3 text-foreground-primary/.75 highlight:text-foreground-primary [&.active]:highlight:text-foreground-primary"}
+                    >
+                        {categoryLabel(category)}
+                    </Link>
+                ))
+            }
+        </nav>
+    );
+}
--- client/src/routes/_with-loader/_layout.tsx
+++ client/src/routes/_with-loader/_layout.tsx
@@ -1,8 +1,6 @@
 import Aside from "@/animejoy/widgets/aside/ui";
-import Container from "@/shared/ui/kit/container";
-import { Link, Outlet, createFileRoute } from "@tanstack/react-router";
-import { useShikimoriLogIn } from "../../features/auth/shikimoriLogIn";
-import { trpc } from "@/shared/api/trpc";
+import Navbar from "@/animejoy/widgets/navbar/ui";
+import { Outlet, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_with-loader/_layout")({
     component: RouteComponent,
@@ -17,24 +15,11 @@ export const Route = createFileRoute("/_with-loader/_layout")({
 });
 
 function RouteComponent() {
-    const { data: user } = trpc.shikimori.users.whoami.useQuery();
-    const { logIn, logOut } = useShikimoriLogIn();
+
 
     return (
         <>
-            <div className={"fixed inset-y-0 left-0 w-header-width p-1.5"}>
-                <Container className={"flex size-full flex-col gap-2 p-2"}>
-                    <Link to={"/"} className={"[&.active]:font-bold"}>
-                        Home
-                    </Link>
-                    <button
-                        onClick={() => (user ? logOut() : logIn())}
-                        className={"[&.active]:font-bold"}
-                    >
-                        {user ? "Sing Out" : "Sign In"}
-                    </button>
-                </Container>
-            </div>
+            <Navbar />
             <main className={"mb-1.5 flex min-h-full flex-col"}>
                 <nav className={"h-breadcrumbs-height"}>breadcrumbs</nav>
                 <Outlet />
