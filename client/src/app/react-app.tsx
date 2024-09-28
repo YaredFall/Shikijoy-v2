684f51ad8b4f4f1bd07fc9acc59990a3e0910e26
YaredFall <yaredfall@gmail.com>
2024-09-28 22:33:35 +0700

    feat(client): reduce number of unwanted retries

--- client/src/app/react-app.tsx
+++ client/src/app/react-app.tsx
@@ -1,14 +1,34 @@
 import AppRouter from "@/app/router";
 import { trpc, trpcClient } from "@/shared/api/trpc/index";
+import type { AppRouter as TRPCAppRouter } from "@server/trpc/routers";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { TRPCClientError } from "@trpc/client";
 import { StrictMode } from "react";
 
+const shouldRetry = (error: unknown) => {
+    if (error instanceof TRPCClientError) {
+        const code = (error as TRPCClientError<TRPCAppRouter>).data?.httpStatus;
+        if (code && (code === 429 || code >= 500)) return true;
+    }
+    return false;
+};
 
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             staleTime: 3_600_000,
-            retry: 1,
+            retry: false,
+            refetchOnMount: (query) => {
+                const error = query.state.error;
+                if (shouldRetry(error)) return true;
+                return false;
+            },
+            refetchOnWindowFocus: (query) => {
+                const error = query.state.error;
+                if (shouldRetry(error)) return true;
+                return false;
+            },
+            // retryOnMount: false,
         },
     },
 });
