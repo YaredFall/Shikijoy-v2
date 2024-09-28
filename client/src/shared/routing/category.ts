413b31e123b3f177558f3dae256c07b21f63faae
YaredFall <yaredfall@gmail.com>
2024-09-29 01:39:31 +0700

    fix(client): categories

--- client/src/shared/routing/category.ts
+++ client/src/shared/routing/category.ts
@@ -7,29 +7,32 @@ export const SHOW_CATEGORIES = [
     "ova",
     "anime-films",
     "anons",
-    "dorams",
+    // "dorams",
 ] as const;
+export type ShowCategory = typeof SHOW_CATEGORIES[number];
 
+export const HOME_AS_CATEGORY = "/";
 export const NEWS_AS_CATEGORY = "news";
 
-export const CATEGORIES = [...SHOW_CATEGORIES, NEWS_AS_CATEGORY] as const;
-export type ShowCategory = typeof CATEGORIES[number];
+export const CATEGORIES = [HOME_AS_CATEGORY, ...SHOW_CATEGORIES, NEWS_AS_CATEGORY] as const;
+export type Category = typeof CATEGORIES[number];
 
 export const KnownCategorySchema = z.enum(CATEGORIES);
 
-const CATEGORY_LABELS: Record<ShowCategory, string> = {
+const CATEGORY_LABELS: Record<Category, string> = {
+    "/": "Главная",
     "tv-serialy": "TV сериалы",
     "ongoing": "Онгоинги",
     "full_tv": "Завершённые",
     "ova": "OVA/ONA/OAV",
     "anime-films": "Аниме фильмы",
     "anons": "Анонсы",
-    "dorams": "Дорамы",
+    // "dorams": "Дорамы",
     "news": "Новости",
 };
 
-export const categoryLabel = (category: ShowCategory | string): string => {
-    return CATEGORY_LABELS[category as ShowCategory];
+export const categoryLabel = (category: Category | string): string => {
+    return CATEGORY_LABELS[category as Category];
 };
 // export const HOME_AS_CATEGORY = { path: "", name: "Главная" } as const satisfies ShowCategory;
 // export const SHOW_CATEGORIES = [
