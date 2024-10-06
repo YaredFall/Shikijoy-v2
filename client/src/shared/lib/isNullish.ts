00f573b48fd4464ba101e63fd027edbf4e562de9
YaredFall <yaredfall@gmail.com>
2024-10-06 21:04:27 +0700

    fix(client): isNullish type guard

--- client/src/shared/lib/isNullish.ts
+++ client/src/shared/lib/isNullish.ts
@@ -1,3 +1,3 @@
-export default function isNullish<T>(value: T): value is T & (undefined | null) {
+export default function isNullish<T>(value: T | undefined | null): value is undefined | null {
     return value === undefined || value === null;
 }
