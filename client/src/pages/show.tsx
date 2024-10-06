dc669e0433e173a26cbd807c404dc4a3e8a8a54e
YaredFall <yaredfall@gmail.com>
2024-10-06 20:47:29 +0700

    fix(client): show popovers were overlayed by aside

--- client/src/pages/show.tsx
+++ client/src/pages/show.tsx
@@ -1,24 +1,7 @@
-// import FranchiseBlock from "@client/components/franchise-block";
-// import Main from "@client/components/layouts/blocks/main/main";
-// import ShowAside from "@client/components/pages/show/aside/show-aside";
-// import Characters from "@client/components/pages/show/characters/characters";
-// import Description from "@client/components/pages/show/description";
-// import Screenshots from "@client/components/pages/show/screenshots";
-// import Container from "@client/components/ui/kit/container";
-// import { getExternalLinks, getFranchise, getShikimoriID } from "@client/entities/animejoy/show/scraping";
-// import { useAnimejoyPage } from "@client/query-hooks/useAnimejoyPage";
-// import { useShikijoyApi } from "@client/query-hooks/useShikijoyApi";
-// import { KnownShowCategory } from "@client/types/animejoy";
-// import { ShikijoyAnimeData } from "@client/types/shikijoy";
-// import { SHIKIJOY_API_ROUTES } from "@client/utils/fetching";
-// import { useMemo } from "react";
-// import { useLocation, useNavigate } from "react-router-dom";
-// import Player from "./player/style3/player";
-
 import Player from "@client/animejoy/entities/show/playlist/ui/player";
 import Characters from "@client/animejoy/entities/show/ui/characters";
-import Description from "@client/animejoy/entities/show/ui/info";
 import FranchiseBlock from "@client/animejoy/entities/show/ui/franchise-block";
+import Description from "@client/animejoy/entities/show/ui/info";
 import Screenshots from "@client/animejoy/entities/show/ui/screenshots";
 import Container from "@client/shared/ui/kit/container";
 import { useLoaderData } from "@tanstack/react-router";
@@ -29,9 +12,11 @@ export default function ShowPage() {
     const { shikimoriAnimeId } = useLoaderData({ from: "/_with-loader/_layout/_animejoy-pages/$category/$showId/" });
 
     return (
-        <Container className={"relative isolate flex-1 bg-background-primary p-0"}>
-            <div className={"relative *:px-6"}>
-                <div className={"absolute inset-0 -bottom-40 -z-10 rounded-[inherit] bg-gradient-to-b from-accent-secondary to-transparent p-0 opacity-10 saturate-[1000%]"} aria-hidden />
+        <Container className={"relative flex-1 bg-background-primary p-0"}>
+            <div className={"relative rounded-[inherit] *:px-6"}>
+                <div className={"pointer-events-none absolute inset-0 -bottom-40 isolate overflow-hidden rounded-[inherit] !p-0"} aria-hidden>
+                    <div className={"size-full bg-gradient-to-b from-accent-secondary to-transparent opacity-10 saturate-[1000%]"}></div>
+                </div>
                 <Description />
                 <FranchiseBlock />
                 <Screenshots />
